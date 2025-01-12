ad73e4fb941a9d4d19bde9bab327cd8619602e17
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-01-11 16:41:06 -0800

    working through practice python basic list calls

---
+++ PythonClassPractice.py
@@ -0,0 +1,16 @@
+#very basic python operations
+# add() extend()--> adds element from another list, (remove(), pop())--> both removes
+# accessing is the same (via index), iterating, loop: for...in..
+listA = []
+
+listA.append("apple")
+print(listA)
+listA.append("orange")
+listA.append(230)
+print(listA)
+print(listA[0])
+print(listA.remove(230))
+print(listA)
+print(len(listA))
+
+
